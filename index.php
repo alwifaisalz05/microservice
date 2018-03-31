9fc01e4f8d21d150214b4d5103a1702b985e71ca
Alwi faisal <31927409+alwifaisalz05@users.noreply.github.com>
2018-03-31 10:42:45 +0700

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 <body>
 	<form action="tampil.php" method="post">
 	
-	<h1>Olah nilai mahasiswa</h1>
+	<h1 style="font-family : Century Gothic;">Olah nilai mahasiswa</h1>
 	UTS : <input type="text" name="uts"><br>
 	UAS : <input type="text" name="uas"><br>
 	QUIZ : <input type="text" name="quiz"><br>
