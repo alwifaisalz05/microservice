3626fed81ee5f3870c8a34b71aa50b66338102bc
Alwi faisal <31927409+alwifaisalz05@users.noreply.github.com>
2018-03-31 10:36:06 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Olah nilai html</title>
+</head>
+<body>
+	<form action="tampil.php" method="post">
+	
+	<h1>Olah nilai mahasiswa</h1>
+	UTS : <input type="text" name="uts"><br>
+	UAS : <input type="text" name="uas"><br>
+	QUIZ : <input type="text" name="quiz"><br>
+	TUGAS : <input type="text" name="tugas"><br>
+	<br>
+	
+    <input type="submit" name="poses">
+
+	</form>
+
+</body>
+</html>
